494cbbe8021d4153e600aac12346ae178b7ea509
Omar Sosa <omar.sosa.quimica@gmail.com>
2024-09-30 14:18:31 -0600

    added feedback to the active path

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import { Link, useNavigate } from "react-router-dom"
+import { NavLink, useNavigate } from "react-router-dom"
 import Burger from "../burger-icon/burger"
 import logo from "../../assets/logo.png"
 import fb from '../../assets/Facebook.png'
@@ -18,10 +18,10 @@ export default function Navbar(){
             <div className="nav">
                 <img src={logo} className="logo" alt="loco icon" onClick={()=> navigate("/")} />
                 <div className="nav-links">
-                    <Link to="/">HOME</Link>
-                    <Link to="menu">MENU</Link>
-                    <Link to="specials">SPECIALS</Link>
-                    <Link to="other">OTHER</Link>
+                    <NavLink className={({isActive}) => isActive ? "link-active" : ""} to="/">HOME</NavLink>
+                    <NavLink className={({isActive}) => isActive ? "link-active" : ""} to="menu">MENU</NavLink>
+                    <NavLink className={({isActive}) => isActive ? "link-active" : ""} to="specials">SPECIALS</NavLink>
+                    <NavLink className={({isActive}) => isActive ? "link-active" : ""} to="other">OTHER</NavLink>
                 </div>
                 <div className="burger-icon">
                     <Burger arial-label="open-close" onClickProp={onClickProp} open={open}/>
@@ -29,10 +29,10 @@ export default function Navbar(){
             </div>
             <div className={`nav-container ${open ? 'open' : 'close'}`}>
                 <div className="nav-container-links">
-                    <Link onClick={()=> setOpen(false)} to="/">HOME</Link>
-                    <Link onClick={()=> setOpen(false)} to="menu">MENU</Link>
-                    <Link onClick={()=> setOpen(false)} to="specials">SPECIALS</Link>
-                    <Link onClick={()=> setOpen(false)} to="other">OTHER</Link>
+                    <NavLink className={({isActive}) => isActive ? "link-active" : ""} onClick={()=> setOpen(false)} to="/">HOME</NavLink>
+                    <NavLink className={({isActive}) => isActive ? "link-active" : ""} onClick={()=> setOpen(false)} to="menu">MENU</NavLink>
+                    <NavLink className={({isActive}) => isActive ? "link-active" : ""} onClick={()=> setOpen(false)} to="specials">SPECIALS</NavLink>
+                    <NavLink className={({isActive}) => isActive ? "link-active" : ""} onClick={()=> setOpen(false)} to="other">OTHER</NavLink>
                 </div>
                 <div className="nav-socials">
                     <a href="#"><img src={ig} alt="instagram logo" /></a>
